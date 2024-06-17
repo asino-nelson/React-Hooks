623d7f8ae930fc70a901de3d57569048eec51800
asino-nelson <asinonelson276@gmail.com>
2024-06-17 15:36:04 +0300

    usestate final commit

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const UseState = () => {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
   return (
     <div>
-    <p>Clicked {count} times</p>
-    <button onClick={()=> setCount(count + 1)} >
-        Click Me
-    </button>
-      
+      <div>
+        <p>Clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Click Me</button>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter name..."
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        <p>Welcome {inputValue}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UseState
+export default UseState;
