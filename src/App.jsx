3d649c70e5da76b52e4dff8373f10f45b93d10cc
asino-nelson <asinonelson276@gmail.com>
2024-06-17 16:05:40 +0300

    UseReducer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import UseReducer from "./UseReducer";
 import UseState from "./UseState";
 
 function App() {
   return (
     <>
       <div>
+        <h1>useState</h1>
         <UseState />
+
+        <h1>useReducer</h1>
+        <UseReducer/>
       </div>
     </>
   );
